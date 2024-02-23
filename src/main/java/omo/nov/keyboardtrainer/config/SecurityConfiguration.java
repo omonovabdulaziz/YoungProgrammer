45351490da95bba503176c5eb214b84c2d49f418
omonovabdulaziz <omonov2006omonov@gmail.com>
2024-02-24 02:10:07 +0500

    fixx

--- src/main/java/omo/nov/keyboardtrainer/config/SecurityConfiguration.java
+++ src/main/java/omo/nov/keyboardtrainer/config/SecurityConfiguration.java
@@ -48,7 +48,16 @@ public class SecurityConfiguration {
                 auth
                         .requestMatchers("/api/v1/**",
                                 "/api/v1/file/**",
+                                "/v2/api-docs",
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-resources",
+                                "/swagger-resources/**",
+                                "/configuration/ui",
+                                "/configuration/security",
                                 "/swagger-ui/**",
+                                "/webjars/**",
+                                "/swagger-ui.html",
                                 "/api/v1/attemptContest/rate/notUser/**",
                                 "/api/v1/regular/getRateNotUser",
                                 "/api/v1/contest/getLastContest"
