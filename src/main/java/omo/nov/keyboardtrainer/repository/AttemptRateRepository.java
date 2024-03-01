a6a7e052d4b0d2153e967a812d5ee2e9b338c685
omonovabdulaziz <omonov2006omonov@gmail.com>
2024-03-01 23:46:29 +0500

    fiexd

--- src/main/java/omo/nov/keyboardtrainer/repository/AttemptRateRepository.java
+++ src/main/java/omo/nov/keyboardtrainer/repository/AttemptRateRepository.java
@@ -16,7 +16,7 @@ public interface AttemptRateRepository extends JpaRepository<AttemptRate, UUID> 
 
     Optional<AttemptRate> findByUserIdAndContestId(Long id, Long contestId);
 
-    @Query(value = "SELECT new omo.nov.keyboardtrainer.payload.AttemptRateDTO(ar.user , ar.trueLetterCount , ar.falseLetterCount , ar.commonTrue , ar.startAt , ar.endAt) FROM AttemptRate ar where ar.contest.id=:contest_id and ar.user.status=true and ar.falseLetterCount < 13 order by ar.commonTrue desc")
+    @Query(value = "SELECT new omo.nov.keyboardtrainer.payload.AttemptRateDTO(ar.user , ar.trueLetterCount , ar.falseLetterCount , ar.commonTrue , ar.startAt , ar.endAt) FROM AttemptRate ar where ar.contest.id=:contest_id and ar.user.status=true and ar.falseLetterCount < 20 order by ar.commonTrue desc")
     Page<AttemptRateDTO> findAllByContestIdOrderByCommonTrue(@Param(value = "contest_id") Long contest_id, Pageable pageable);
 
     List<AttemptRate> findAllByContestIdAndUser_StatusAndFalseLetterCountLessThanOrderByCommonTrueDesc(Long contest_id, Boolean user_status, Integer falseLetterCount);
