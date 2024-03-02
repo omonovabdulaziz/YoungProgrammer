476abd7428d183cab233bdc354a137338d0a85e1
omonovabdulaziz <omonov2006omonov@gmail.com>
2024-03-02 23:56:49 +0500

    fixedd

--- src/main/java/omo/nov/keyboardtrainer/KeyboardTrainerApplication.java
+++ src/main/java/omo/nov/keyboardtrainer/KeyboardTrainerApplication.java
@@ -3,10 +3,14 @@ package omo.nov.keyboardtrainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
 @SpringBootApplication
 public class KeyboardTrainerApplication {
     public static void main(String[] args) {
         SpringApplication.run(KeyboardTrainerApplication.class, args);
-
     }
 }
